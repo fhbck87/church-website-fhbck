c9e92d90bebea7ba55d98d65742f8a46cbb4b14d
fhbck87 <rodriguemedor@yahoo.fr>
2026-09-13 09:44:57 -0400

    test church.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// This file is intentionally kept Node-safe because some hosting providers
+// execute the package entry directly. The browser app is bundled from
+// src/client-index.js via config-overrides.js.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (typeof document !== 'undefined') {
+  console.warn(
+    'The browser entry is loaded via src/client-index.js. This Node-safe stub should not render the app.'
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+module.exports = {};
